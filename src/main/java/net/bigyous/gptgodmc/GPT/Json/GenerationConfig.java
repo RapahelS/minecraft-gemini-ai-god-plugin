5d1f1bae59d5da52a236944c8dd5e35b447ac55d
ConnorBP <connor.postma@gmail.com>
2024-10-28 13:13:11 -0400

    disabled unused generation config options

--- src/main/java/net/bigyous/gptgodmc/GPT/Json/GenerationConfig.java
+++ src/main/java/net/bigyous/gptgodmc/GPT/Json/GenerationConfig.java
@@ -1,15 +1,15 @@
 package net.bigyous.gptgodmc.GPT.Json;
 
 public class GenerationConfig {
-    String[] stopSequences;
-    String responseMimeType; // ex application/json
-    Schema responseSchema;
-    int candidateCount;
-    int maxOutputTokens;
-    double temperature;
-    double topP;
-    int topK;
-    double presencePenalty;
+    // String[] stopSequences;
+    // String responseMimeType;// ex application/json
+    // Schema responseSchema;
+    // int candidateCount;
+    // int maxOutputTokens;
+    double temperature = 1.0;
+    // double topP;
+    // int topK;
+    // double presencePenalty;
 
     public GenerationConfig(double temp) {
         this.temperature = temp;
