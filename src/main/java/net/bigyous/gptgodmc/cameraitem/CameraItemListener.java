f1eb1f5940b45a696b22e32672aed95e7d0d874f
ConnorBP <connor.postma@gmail.com>
2024-11-05 19:09:25 -0500

    forgot to set lastTime

--- src/main/java/net/bigyous/gptgodmc/cameraitem/CameraItemListener.java
+++ src/main/java/net/bigyous/gptgodmc/cameraitem/CameraItemListener.java
@@ -26,26 +26,31 @@ public class CameraItemListener implements Listener {
     static final int MIN_PHOTO_DELY_SECONDS = 5;
     // rate limiter
     Map<UUID, Instant> lastPlayerPhoto = new HashMap<>();
-    
+
     @EventHandler
     public void onClick(final PlayerInteractEvent event) {
         // only accept right click if primary hand
         // 1.9+ fires once for left hand and once for right hand
-        if(event.getHand() != EquipmentSlot.HAND || event.getAction() != Action.RIGHT_CLICK_AIR) return;
+        if (event.getHand() != EquipmentSlot.HAND || event.getAction() != Action.RIGHT_CLICK_AIR)
+            return;
 
         Player player = event.getPlayer();
         Instant lastTime = lastPlayerPhoto.get(player.getUniqueId());
         Instant currentTime = Instant.now();
-        if(lastTime == null || Duration.between(lastTime, currentTime).getSeconds() > MIN_PHOTO_DELY_SECONDS) {
+        if (lastTime == null || Duration.between(lastTime, currentTime).getSeconds() > MIN_PHOTO_DELY_SECONDS) {
             ItemStack hand = player.getInventory().getItemInMainHand();
-        // only run on valid camera items (has hidden meta)
-        if(!GiveCameraCommand.isItemValidCamera(hand)) return;
+            // only run on valid camera items (has hidden meta)
+            if (!GiveCameraCommand.isItemValidCamera(hand))
+                return;
             GPTGOD.LOGGER.info(String.format("%s is trying to take a picture with the camera item", player.getName()));
             // snap a picture from the current players view
             ImageUtils.takePicture(player);
             player.sendMessage("Snapped a photo for god");
+            // set the last time a photo was taken
+            lastTime = Instant.now();
         } else {
-            player.sendMessage(String.format("You may only take a picture every %d seconds.", MIN_PHOTO_DELY_SECONDS) + ChatColor.RED);
+            player.sendMessage(String.format("You may only take a picture every %d seconds.", MIN_PHOTO_DELY_SECONDS)
+                    + ChatColor.RED);
         }
     }
 }
